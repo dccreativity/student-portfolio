c08e38e270fc9ec1d083f1696739bd88def6f04a
dccreativity <132959332+dccreativity@users.noreply.github.com>
2026-09-04 15:15:41 +0530

    Delete student-portfolio directory

--- student-portfolio/components/BreathingBackground.js
+++
@@ -1,8 +0,0 @@
-export default function BreathingBackground() {
-  return (
-    <div className="breathing-bg" aria-hidden="true">
-      <span className="blob-1" />
-      <span className="blob-2" />
-    </div>
-  );
-}
